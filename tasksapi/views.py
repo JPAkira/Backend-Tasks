9afc10a9688ce36f0d88f927d0d9b08cfa97422e
JPAkira <joaopauloribeiromyszka@gmail.com>
2023-02-19 05:09:58 -0300

    docker build

--- tasksapi/views.py
+++ tasksapi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from tasksapi import serializers
+from tasksapi.models import Task
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = serializers.TaskSerializer
+    permission_classes = [IsAuthenticated]
+
