08b789296d9341accb4b97be1c8588e481538c05
JPAkira <joaopauloribeiromyszka@gmail.com>
2023-02-22 03:34:44 -0300

    add user field and change priority types

--- tasksapi/models.py
+++ tasksapi/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class PriorityType(models.IntegerChoices):
-    BAIXA = 0
-    MEDIA = 1
-    ALTA = 2
+    UMA_ESTRELA = 1
+    DUAS_ESTRELA = 2
+    TRES_ESTRELA = 3
+    QUATRO_ESTRELA = 4
+    CINCO_ESTRELA = 5
+
 
 class StatusType(models.IntegerChoices):
     PENDENTE = 0
@@ -14,10 +18,11 @@ class StatusType(models.IntegerChoices):
 
 class Task(models.Model):
     name = models.CharField(max_length=50)
-    priority = models.IntegerField(choices=PriorityType.choices, default=PriorityType.MEDIA, db_index=True)
+    priority = models.IntegerField(choices=PriorityType.choices, default=PriorityType.TRES_ESTRELA, db_index=True)
     status = models.IntegerField(choices=StatusType.choices, default=StatusType.PENDENTE, db_index=True)
     event_at = models.DateTimeField(db_index=True)
     finish_at = models.DateTimeField(db_index=True)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
