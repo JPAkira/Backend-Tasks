08fe8ade8fe416c22093ca5b456d86890fd01f55
JPAkira <joaopauloribeiromyszka@gmail.com>
2023-02-22 04:51:22 -0300

    add cors headers

--- tasks/urls.py
+++ tasks/urls.py
@@ -12,8 +12,8 @@ router = routers.SimpleRouter()
 router.register(r"tasks", TaskViewSet, basename="tasks")
 
 urlpatterns = [
-    path("api/", include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("backend/api/", include(router.urls)),
+    path('backend/admin/', admin.site.urls),
+    path('backend/api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('backend/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
