48c8bb9b46155c046af97c795e0bf3d6f9084e14
JPAkira <joaopauloribeiromyszka@gmail.com>
2023-02-22 06:25:36 -0300

    add request user to post

--- tasksapi/serializers.py
+++ tasksapi/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import CurrentUserDefault
 
 from tasksapi.models import Task
 
@@ -7,3 +8,10 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
+
+    def create(self, validated_data):
+        task = Task.objects.create(
+            user=self.context['request'].user,
+            **validated_data
+        )
+        return task
