443fdcb021b0f732601081059797ca0e7e060532
JPAkira <joaopauloribeiromyszka@gmail.com>
2023-02-22 05:41:32 -0300

    change to priority low / average / high

--- tasks/settings.py
+++ tasks/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -96,6 +97,10 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=36),
 }
 
 CORS_ALLOWED_ORIGINS = [
